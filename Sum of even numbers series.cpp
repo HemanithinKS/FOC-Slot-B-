f22c98b590c12cc9760cd09b76dd75ed4a2abcf6
Hemanithin K S <113330982+HemanithinKS@users.noreply.github.com>
2022-09-19 09:38:01 +0530

    Add files via upload

---
+++ Sum of even numbers series.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>  
+int main()  
+{  
+    int i,n, sum=0;  
+    printf("Enter the number: ");  
+    scanf("%d",&n);  
+    sum=n*(n+1);
+    printf("sum of square of %d natural numbers=%d",n,sum); 
+    return 0;  
+}  
